4ffbc8cc71095f6106e512011de86702d59cd3b9
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2022-05-12 17:35:59 +0100

    terrabutler: Add utils.py

---
+++ terrabutler/utils.py
@@ -0,0 +1,12 @@
+from os import getenv
+
+ROOT_PATH = getenv("TERRABUTLER_ROOT")
+
+paths = {
+    "root": ROOT_PATH,
+    "backends": ROOT_PATH + "/configs/backends",
+    "templates": ROOT_PATH + "/configs/templates",
+    "variables": ROOT_PATH + "/configs/variables",
+    "environment": ROOT_PATH + "/site_inception/.terraform/environment",
+    "inception": ROOT_PATH + "/inception",
+}
