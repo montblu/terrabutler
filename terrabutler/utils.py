d6568a97d9b25d2b1941cc4e2a118d6b55c6ee96
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2022-05-13 15:52:12 +0100

    terrabutler: utils.py: Add is_semantic_version

--- terrabutler/utils.py
+++ terrabutler/utils.py
@@ -1,4 +1,6 @@
 from terrabutler.requirements import check_requirements
+from sys import exit
+from semantic_version import Version
 from os import getenv
 
 check_requirements()
@@ -13,3 +15,17 @@ paths = {
     "templates": ROOT_PATH + "/configs/templates",
     "variables": ROOT_PATH + "/configs/variables"
 }
+
+
+def is_semantic_version(version):
+    """
+    Check if the version corresponds to the semantic versioning.
+    """
+    try:
+        Version(version)
+    except ValueError:
+        return False
+    except Exception as e:
+        print(f"There was an error while parsing version: {e}")
+        exit(1)
+    return True
