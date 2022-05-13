a2df6347c0f30202347886d7b45ece516cb7ca18
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2022-05-13 11:13:07 +0100

    terrabutler: utils.py: Add check_reqirements()

--- terrabutler/utils.py
+++ terrabutler/utils.py
@@ -1,12 +1,8 @@
-from colorama import Fore
+from terrabutler.requirements import check_requirements
 from os import getenv
 
+check_requirements()
 ROOT_PATH = getenv("TERRABUTLER_ROOT")
-if getenv("TERRABUTLER_ENABLE") != "true":
-    print(Fore.YELLOW + "Terrabutler is not currently enabled on this"
-          " folder. Please set 'TERRABUTLER_ENABLE' in your environment"
-          " to true to enable it." + Fore.RESET)
-    exit(1)
 
 paths = {
     "backends": ROOT_PATH + "/configs/backends",
