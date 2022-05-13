8e415c295911b48a7a9258025c7e39edc978de3e
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2022-05-13 11:08:26 +0100

    terrabutler: requirements.py: Fix reset of color after print

--- terrabutler/requirements.py
+++ terrabutler/requirements.py
@@ -9,17 +9,18 @@ def check_requirements():
     if getenv("TERRABUTLER_ENABLE") != "true":
         print(Fore.YELLOW + "Terrabutler is not currently enabled on this"
               " folder. Please set 'TERRABUTLER_ENABLE' in your environment"
-              " to true to enable it.")
+              " to true to enable it." + Fore.RESET)
         exit(1)
     root = getenv("TERRABUTLER_ROOT")
     if root is None or not path.exists(root):
         print(Fore.RED + "Terrabutler can't determine the root folder of"
               " your project or it doesn't exist.\nPlease set"
               " 'TERRABUTLER_ROOT' in your environment pointing"
-              " to the root folder of your project.")
+              " to the root folder of your project." + Fore.RESET)
         exit(1)
     if not path.exists(root + "configs/settings.yml"):
         print(Fore.RED + "Terrabutler can't find you settings file\nPlease"
-              " create a 'settings.yml' file inside the 'configs' folder.")
+              " create a 'settings.yml' file inside the 'configs' folder."
+              + Fore.RESET)
         exit(1)
     validate_settings()
